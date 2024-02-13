2a4d96dc2f702218229ccc5451ba0a3e531acb20
JorgeBracero <jorgebramar@gmail.com>
2024-02-14 00:40:46 +0100

    ControlLogin
    otro commit beibe

--- Stars Casino/loginCorrecto.php
+++ Stars Casino/loginCorrecto.php
@@ -1,7 +1,18 @@
 <?php
-$usuario = $_POST['usuario'];
-$password = $_POST['contraseña'];
-#jorge guapo
-echo "usuario: ". $usuario. "<br>";
-echo "psw: ". $password;
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){ //Si el metodo de nuestro formulario es POST
+    
+    //El usuario y la contraseña nunca van a estar vacíos
+    $usuario = $_POST['usuario'];
+    $password = $_POST['contraseña'];
+
+    //Código comprobación login en la BD
+
+
+    //---------------------------------------------------
+    //Si el login es correcto nos envia a la página de inicio
+    header("Location: inicio.html");
+    exit();
+}
+
 ?>
