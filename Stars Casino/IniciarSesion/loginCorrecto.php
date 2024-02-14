f32187574896c5761543fb81e0ffdbafb81a661c
JorgeBracero <jorgebramar@gmail.com>
2024-02-14 15:56:54 +0100

    loginCorrecto2
    ywa

--- Stars Casino/IniciarSesion/loginCorrecto.php
+++ Stars Casino/IniciarSesion/loginCorrecto.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:Stars Casino/IniciarSesion/loginCorrecto.php
 <?php
 
 include 'conexion.php'; //Importamos el fichero de conexion con la BD php
@@ -32,4 +33,42 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){ //Si el metodo de nuestro formulario e
     }
 }
 
+=======
+<?php
+
+include 'conexion.php'; //Importamos el fichero de conexion con la BD php
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){ //Si el metodo de nuestro formulario es POST
+    
+    //El usuario y la contraseña nunca van a estar vacíos
+    $correo = $_POST['correo'];
+    $password = $_POST['contraseña'];
+    $loginCorrecto = false;
+
+    //Código comprobación login en la BD
+    $ins = "SELECT * FROM usuarios WHERE Correo = '".$correo."' AND
+         Contraseña = '".$password."'";
+
+    $res = mysqli_query($con,$ins);
+
+    if($res){
+        if(mysqli_num_rows($res) > 0){ //Si la consulta es correcta y nos devuelve algún registro, el usuario existe
+            $loginCorrecto = true;
+        }else{
+            echo "<h2>EL USUARIO NO EXISTE PVRRA</h2>";
+        }
+    }
+    
+    //Cerramos la conexión
+    mysqli_close($con);
+
+    //---------------------------------------------------
+    //Si el login es correcto nos envia a la página de inicio
+    if($loginCorrecto){
+        header("Location: inicio.html");
+        exit();
+    }
+}
+
+>>>>>>> Stashed changes:Stars Casino/loginCorrecto.php
 ?>
