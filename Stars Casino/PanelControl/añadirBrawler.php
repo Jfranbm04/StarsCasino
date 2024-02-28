e0068f0f9731cd6dfbf57ad0cc4a8356728ab58a
JorgeBracero <jorgebramar@gmail.com>
2024-02-28 04:25:05 +0100

    CRUD
    muy fresco pana

---
+++ Stars Casino/PanelControl/añadirBrawler.php
@@ -0,0 +1,54 @@
+<?php
+
+include '../conexion.php'; //Importamos el fichero de conexion con la BD php
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){ //Si el metodo de nuestro formulario es POST
+    
+    //Los datos siempre van a tener contenido
+    $nombre = $_POST['nombre'];
+    $calidad = $_POST['calidad'];
+    $nivel = $_POST['nivel'];
+    $brawlCorrecto = false;
+
+    //Código insertar brawler en la BD
+    $ins = "INSERT INTO brawler (Nombre,Calidad,Nivel) VALUES 
+        ('$nombre','$calidad','$nivel')";
+
+    try{
+        mysqli_query($con,$ins); //Ejecutamos el insert, si ya existe el brawler muestra un mensaje de error
+        $brawlCorrecto = true;
+    }catch(Exception $ex){
+        echo "<html>";
+        echo "<head>";
+        echo "<title>ERROR AÑADIR BRAWLER</title>";
+        echo "<style>";
+        echo "h2,h3 { color: white; }";
+        echo ".container { 
+            background-color: red;
+            border: solid 3px;
+            padding: 2%;
+        }";
+        echo "</style>";
+        echo "</head>";
+        echo "<body>";
+        echo "<div class=\"container\">";
+        echo "<h2>EL BRAWLER YA ESTA EN LA COLECCIÓN</h2><br>". "<h3><b>ERROR: ".$ex->getMessage()."</b></h3>";
+        echo "</div>";
+        echo "</body>";
+        echo "</html>";
+        
+    }finally{
+        //Cerramos la conexión, ocurra o no algún error
+        mysqli_close($con);
+    }
+
+    //---------------------------------------------------
+    //Si el brawler es nuevo en la coleccion, nos envia al panel de control
+    if($brawlCorrecto){
+        header("Location: panelControl.html");
+        exit();
+    }
+}
+
+
+?>
---
+++ Stars Casino/PanelControl/borrarBrawler.php
@@ -0,0 +1,55 @@
+<?php
+
+include '../conexion.php'; //Importamos el fichero de conexion con la BD php
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){ //Si el metodo de nuestro formulario es POST
+    
+    //El nombre nunca va a estar vacio
+    $nombre = $_POST['nombre'];
+    $borradoCorrecto = false;
+
+    //Código comprobación login en la BD
+    $ins = "DELETE FROM brawler WHERE Nombre = '$nombre'";
+
+    $res = mysqli_query($con,$ins);
+
+    if($res){
+        // Verificamos si se afectaron filas (si se borró algún registro)
+        $filasAfectadas = mysqli_affected_rows($con);
+    
+        if ($filasAfectadas > 0) {
+            $borradoCorrecto = true; // Si el delete es correcto, se afectaron filas y el brawler existe en nuestra colección
+        } else {
+            echo "<html>";
+            echo "<head>";
+            echo "<title>ERROR BORRAR BRAWLER</title>";
+            echo "<style>";
+            echo "h2 { color: white; }";
+            echo ".container { 
+                background-color: red;
+                border: solid 3px;
+                padding: 2%;
+            }";
+            echo "</style>";
+            echo "</head>";
+            echo "<body>";
+            echo "<div class=\"container\">";
+            echo "<h2>EL BRAWLER NO EXISTE EN LA COLECCIÓN</h2>";
+            echo "</div>";
+            echo "</body>";
+            echo "</html>";
+        }
+    }
+
+    // Cerramos la conexión
+    mysqli_close($con);
+
+    //---------------------------------------------------
+    //Si el borrado es correcto nos envia al panel de Control
+    if($borradoCorrecto){
+        header("Location: panelControl.html");
+        exit();
+    }
+}
+
+?>
